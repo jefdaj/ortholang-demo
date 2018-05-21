e022e30cbd9827afe70c74e73ae50b8d0ef2d4c9
Jeffrey David Johnson <jefdaj@berkeley.edu>
2018-05-20 17:40:37 -0700

    remove persistent sessions for now

--- shortcut.py
+++ shortcut.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python2
 
 # TODO thank that guy for the random numbers example that fixed the bug
-# TODO broadcast to clients individually rather than everyone at once
 # TODO repl should respawn when it crashes or is exited
 
 from datetime       import datetime
 from flask          import Flask, render_template, request, make_response
-from flask_login    import LoginManager
-from flask_session  import Session
 from flask_socketio import SocketIO, emit
 from os.path        import join, realpath
 from re             import sub
@@ -30,8 +27,7 @@ def log(msg):
 # shortcut threads #
 ####################
 
-# TODO is this good enough, or is an actual lock still needed?
-shortcut_threads = {}
+threads = {}
 
 class ShortcutThread(Thread):
     def __init__(self, sessionid):
@@ -49,13 +45,16 @@ class ShortcutThread(Thread):
 
     def run(self):
         # TODO handle restarting the process when killed here?
-        self.emitStdoutLines()
+        self.emitStdout()
 
-    def writeStdinLine(self, line):
-        socketio.emit('replstdout', '>> ' + line + '\n', namespace='/')
+    def emitLine(self, line):
+        socketio.emit('replstdout', line, namespace='/', room=self.sessionid)
+
+    def readLine(self, line):
+        self.emitLine('>> ' + line + '\n')
         self.process.stdin.write(line + '\n')
 
-    def emitStdoutLines(self):
+    def emitStdout(self):
         while True:
             sleep(self.delay)
             try:
@@ -63,64 +62,41 @@ class ShortcutThread(Thread):
             except:
                 continue
             if line:
-                # TODO fix 
-                # log("emitting line: '%s'" % line.strip())
                 line = sub(r'^(>> )*', '', line)
-                socketio.emit('replstdout', line, namespace='/')
+                self.emitLine(line)
 
-###################
-# flask webserver #
-###################
+#############
+# webserver #
+#############
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'so-secret!'
-login = LoginManager(app)
-Session(app)
 socketio = SocketIO(app, manage_session=False, logger=True, engineio_logger=True)
 
 @app.route('/')
 def index():
-    # cookie is set here because we're making an http response anyway
-    resp = make_response(render_template('index.html'))
-    ssid = get_sessionid()
-    if not 'shortcut-session-id' in request.cookies:
-        resp.set_cookie('shortcut-session-id', ssid)
-    return resp
-
-def get_sessionid():
-    # cookie should have been see in index() already, unless called from there
-    try:
-        return request.cookies['shortcut-session-id']
-    except:
-        ssid = uuid4().hex
-        return ssid
+    return render_template('index.html')
 
 @socketio.on('connect')
 def handle_connect():
-    # cookie should have been set in index() already
-    ssid = get_sessionid()
-    print('client %s connected' % ssid)
-    global shortcut_threads
-    if not ssid in shortcut_threads:
-        shortcut_threads[ssid] = ShortcutThread(ssid)
-    thread = shortcut_threads[ssid]
+    sid = request.sid
+    print('client %s connected' % sid)
+    global threads
+    if not sid in threads:
+        threads[sid] = ShortcutThread(sid)
+    thread = threads[sid]
     if not thread.isAlive():
         thread.start()
 
 @socketio.on('replstdin')
 def handle_replstdin(line):
-    ssid = get_sessionid()
-    repl = shortcut_threads[ssid]
-    log("passing line '%s' to repl %s" % (line, repl))
-    ssid = get_sessionid()
-    thread = shortcut_threads[ssid]
-    thread.writeStdinLine(line)
+    threads[request.sid].readLine(line)
 
 @socketio.on('comment')
 def handle_comment(comment):
-    ssid = get_sessionid()
-    log("client %s submitted a comment: '%s'" % (ssid, comment))
-    filename = join('comments', '%s_%s.txt' % (timestamp(), ssid))
+    sid = request.sid
+    log("client %s submitted a comment: '%s'" % (sid, comment))
+    filename = join('comments', '%s_%s.txt' % (timestamp(), sid))
     with open(filename, 'w') as f:
         f.write(comment.encode('utf-8'))
 
