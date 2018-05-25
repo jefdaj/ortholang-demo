385a9752f328b19efa9aab2cd560a01c315f918a
Jeffrey David Johnson <jefdaj@berkeley.edu>
2018-05-25 13:02:19 -0700

    use some random gist to run under twisted wsgi

--- shortcut.py
+++ shortcut.py
@@ -181,7 +181,31 @@ def handle_comment(comment):
     with open(filename, 'w') as f:
         f.write(comment.encode('utf-8'))
 
-if __name__ == '__main__':
-    # socketio.run(app, debug=True)
-    # this appears to work with twisted + socketio, since we don't want actual websockets:
-    app.run()
+# if __name__ == '__main__':
+#     # socketio.run(app, debug=True)
+#     # this appears to work with twisted + socketio, since we don't want actual websockets:
+#     app.run()
+
+# from https://gist.github.com/ianschenck/977379a91154fe264897
+if __name__ == "__main__":
+    reactor_args = {}
+
+    def run_twisted_wsgi():
+        from twisted.internet import reactor
+        from twisted.web.server import Site
+        from twisted.web.wsgi import WSGIResource
+
+        resource = WSGIResource(reactor, reactor.getThreadPool(), app)
+        site = Site(resource)
+        reactor.listenTCP(5000, site)
+        reactor.run(**reactor_args)
+
+        if app.debug:
+            # Disable twisted signal handlers in development only.
+            reactor_args['installSignalHandlers'] = 0
+            # Turn on auto reload.
+            import werkzeug.serving
+            run_twisted_wsgi = werkzeug.serving.run_with_reloader(run_twisted_wsgi)
+    
+    run_twisted_wsgi()
+
